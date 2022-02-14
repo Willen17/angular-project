14abfb21b4caff470e69ce2a7773ea557c88e74b
William Saar <williamsaar@outlook.com>
2022-02-14 23:34:42 +0100

    complete todo list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodosComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TodosComponent],
+  imports: [BrowserModule, FormsModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCross } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-todos',
@@ -7,24 +10,40 @@ import { Todo } from 'src/app/models/Todo';
   styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
+  faTrash = faTrash;
+  faCheck = faCheck;
+  faCross = faCross;
   constructor() {}
 
   toDos?: Todo[];
 
+  inputToDo?: string = '';
+
   ngOnInit(): void {
-    this.toDos = [
-      {
-        content: 'My first toDo',
-        completed: false,
-      },
-      {
-        content: 'Learn Angular-basics',
-        completed: false,
-      },
-      {
-        content: 'Finish my report about Angular',
-        completed: false,
-      },
-    ];
+    this.toDos = [];
+  }
+
+  toggleDone(id: number) {
+    this.toDos?.map((value, index) => {
+      if (index == id) value.completed = !value.completed;
+
+      return value;
+    });
+  }
+
+  deleteToDo(id: number) {
+    // This function checks if Index isnt equal to the ID. If so, we are not going to return it.
+    this.toDos = this.toDos?.filter((value, index) => index !== id);
+  }
+
+  addToDo() {
+    //Here we push the input value to the toDo array.
+    this.toDos?.push({
+      content: this.inputToDo,
+      completed: false,
+    });
+
+    // This sets the input field to blank afterwards.
+    this.inputToDo = '';
   }
 }
